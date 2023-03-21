afddb60421dcb711d3984c79865f4bd0f8938c59
oliviawu77 <jr5566i@yahoo.com.tw>
2023-03-22 02:08:22 +0800

    remove unused function in image_opencv.cpp

--- src/http_stream.cpp
+++ src/http_stream.cpp
@@ -310,20 +310,6 @@ void send_json_custom(char const* send_buf, int port, int timeout)
     }
 }
 
-void send_json(detection *dets, int nboxes, int classes, char **names, long long int frame_id, int port, int timeout)
-{
-    try {
-        char *send_buf = detection_to_json(dets, nboxes, classes, names, frame_id, NULL);
-
-        send_json_custom(send_buf, port, timeout);
-        std::cout << " JSON-stream sent. \n";
-
-        free(send_buf);
-    }
-    catch (...) {
-        cerr << " Error in send_json() function \n";
-    }
-}
 // ----------------------------------------
 
 
