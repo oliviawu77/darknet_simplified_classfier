64dc9e711e266f648da79091c62bbf739c8ff0a2
oliviawu77 <jr5566i@yahoo.com.tw>
2023-03-23 01:17:03 +0800

    remove unused function in avgpool_layer.c

--- src/avgpool_layer.c
+++ src/avgpool_layer.c
@@ -21,20 +21,8 @@ avgpool_layer make_avgpool_layer(int batch, int w, int h, int c)
     l.delta = (float*)xcalloc(output_size, sizeof(float));
     l.forward = forward_avgpool_layer;
     l.backward = backward_avgpool_layer;
-    #ifdef GPU
-    l.forward_gpu = forward_avgpool_layer_gpu;
-    l.backward_gpu = backward_avgpool_layer_gpu;
-    l.output_gpu  = cuda_make_array(l.output, output_size);
-    l.delta_gpu   = cuda_make_array(l.delta, output_size);
-    #endif
-    return l;
-}
 
-void resize_avgpool_layer(avgpool_layer *l, int w, int h)
-{
-    l->w = w;
-    l->h = h;
-    l->inputs = h*w*l->c;
+    return l;
 }
 
 void forward_avgpool_layer(const avgpool_layer l, network_state state)
--- src/avgpool_layer.h
+++ src/avgpool_layer.h
@@ -12,7 +12,6 @@ extern "C" {
 #endif
 image get_avgpool_image(avgpool_layer l);
 avgpool_layer make_avgpool_layer(int batch, int w, int h, int c);
-void resize_avgpool_layer(avgpool_layer *l, int w, int h);
 void forward_avgpool_layer(const avgpool_layer l, network_state state);
 void backward_avgpool_layer(const avgpool_layer l, network_state state);
 
