be920eab64264fcfcbb5a8afddec04f724e06bed
oliviawu77 <jr5566i@yahoo.com.tw>
2023-03-23 01:51:30 +0800

    remove unused function in gemm.c

--- src/gemm.h
+++ src/gemm.h
@@ -8,21 +8,6 @@ extern "C" {
 #endif
 
 
-static inline void set_bit(unsigned char *const dst, size_t index) {
-    size_t dst_i = index / 8;
-    int dst_shift = index % 8;
-    dst[dst_i] |= 1 << dst_shift;
-    //dst[dst_i] |= 1 << (8 - dst_shift);
-}
-
-static inline unsigned char get_bit(unsigned char const*const src, size_t index) {
-    size_t src_i = index / 8;
-    int src_shift = index % 8;
-    unsigned char val = (src[src_i] & (1 << src_shift)) > 0;
-    //unsigned char val = (src[src_i] & (1 << (8 - src_shift))) > 0;
-    return val;
-}
-
 int is_avx();
 int is_fma_avx2();
 
@@ -44,19 +29,6 @@ void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA,
         float BETA,
         float *C, int ldc);
 
-#ifdef GPU
-void gemm_ongpu(int TA, int TB, int M, int N, int K, float ALPHA,
-        float *A_gpu, int lda,
-        float *B_gpu, int ldb,
-        float BETA,
-        float *C_gpu, int ldc);
-
-void gemm_gpu(int TA, int TB, int M, int N, int K, float ALPHA,
-        float *A, int lda,
-        float *B, int ldb,
-        float BETA,
-        float *C, int ldc);
-#endif
 #ifdef __cplusplus
 }
 #endif
