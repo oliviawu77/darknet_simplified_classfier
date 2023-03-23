418a4612e416be371b2eb06548fe58d70bb0e39b
oliviawu77 <jr5566i@yahoo.com.tw>
2023-03-23 12:38:43 +0800

    remove unused part in http_stream.cpp

--- src/http_stream.cpp
+++ src/http_stream.cpp
@@ -1,50 +1,5 @@
 #define _XOPEN_SOURCE
-#include "image.h"
 #include "http_stream.h"
-
-//
-// a single-threaded, multi client(using select), debug webserver - streaming out mjpg.
-//  on win, _WIN32 has to be defined, must link against ws2_32.lib (socks on linux are for free)
-//
-
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <memory>
-#include <mutex>
-#include <thread>
-#include <atomic>
-#include <ctime>
-using std::cerr;
-using std::endl;
-
-//
-// socket related abstractions:
-//
-
-#include "darkunistd.h"
-#include <fcntl.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <signal.h>
-#define PORT        unsigned short
-#define SOCKET    int
-#define HOSTENT  struct hostent
-#define SOCKADDR    struct sockaddr
-#define SOCKADDR_IN  struct sockaddr_in
-#define ADDRPOINTER  unsigned int*
-#ifndef INVALID_SOCKET
-#define INVALID_SOCKET -1
-#endif
-#ifndef SOCKET_ERROR
-#define SOCKET_ERROR   -1
-#endif
-
-// ----------------------------------------
-
-
-#if __cplusplus >= 201103L || _MSC_VER >= 1900  // C++11
 
 #include <chrono>
 #include <iostream>
@@ -54,24 +9,9 @@ static double total_time;
 
 double get_time_point() {
     std::chrono::steady_clock::time_point current_time = std::chrono::steady_clock::now();
-    //uint64_t now = std::chrono::duration_cast<std::chrono::milliseconds>(current_time.time_since_epoch()).count();
     return std::chrono::duration_cast<std::chrono::microseconds>(current_time.time_since_epoch()).count();
 }
 
-
-#else // C++11
-#include <iostream>
-
-double get_time_point() { return 0; }
-void start_timer() {}
-void stop_timer() {}
-double get_time() { return 0; }
-void stop_timer_and_show() {
-    std::cout << " stop_timer_and_show() isn't implemented " << std::endl;
-}
-void stop_timer_and_show_name(char *name) { stop_timer_and_show(); }
-void total_time() {}
-#endif // C++11
 
 
 
--- src/http_stream.h
+++ src/http_stream.h
@@ -5,20 +5,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include "image.h"
-#include <stdint.h>
-
-
-#ifdef OPENCV
-void send_mjpeg(mat_cv* mat, int port, int timeout, int quality);
-
-int send_http_post_request(char *http_post_host, int server_port, const char *videosource,
-    detection *dets, int nboxes, int classes, char **names, long long int frame_id, int ext_output, int timeout);
-
-#endif  // OPENCV
-
-typedef void* custom_thread_t;
-typedef void* custom_attr_t;
 
 #ifdef __cplusplus
 }
